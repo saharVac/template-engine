0390873ba1e732355dc0583a1e3555581ad1f1cf
saharVac <vcsahar@gmail.com>
2020-10-26 17:57:14 -0400

    Added html file creation feature

--- app.js
+++ app.js
@@ -81,29 +81,21 @@ async function takeInfo() {
 async function main() {
 
   await takeInfo();
-  console.log(employees)
 
   // After the user has input all employees desired, call the `render` function (required above) and pass in an array containing all employee objects; the `render` function will generate and return a block of HTML including templated divs for each employee!
-  const skeleton = render(employees);
-  console.log(skeleton)
+  const page = render(employees);
+  
+    
+  // After you have your html, you're now ready to create an HTML file using the HTML returned from the `render` function. Now write it to a file named `team.html` in the `output` folder. You can use the variable `outputPath` above target this location.
+  fs.writeFile(`./output/team.html`, page, function(err) {
+    if (err) {
+      return console.log(err)
+    }
+  })
+
 }
 
 main()
 
 
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
